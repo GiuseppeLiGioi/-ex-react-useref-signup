043e574833efbe83373ea277052c4b48a9f49b75
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-05-28 09:44:43 +0200

    added. milestone 1

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -28,11 +28,56 @@ Al submit, se il form è valido, stampa in console i dati.
 import { useState } from 'react'
 
 function App() {
- const [name, setName] = useState("")
+ const [name, setName] = useState("");
+ const [username, setUsername] = useState("");
+ const [password, setPassword] = useState("");
+ const [specializzazione, setSpecializzazione] = useState("")
+ const [anni, setAnni] = useState("")
+ const [descr, setDescr] = useState("")
+
+
+ const handleSubmit = (e) => {
+ e.preventDefault()
+ if(name === "" || username === "" || password === "" || specializzazione === "" || anni === "" || descr === "" ){
+  console.log("Devi compilare tutti i campi");
+  return;
+ }else if(parseInt(anni) < 1){
+  console.log("Gli anni di esperienza deve essere un numero positivo")
+  return;
+ }
+ console.log(name, username, password, specializzazione, anni, descr);
+ }
+
+
 
   return (
     <>
-      
+      <form onSubmit={handleSubmit}>
+         <label className='label'>Nome:</label>
+         <input className='input' type='text' placeholder='inserisci il tuo nome' onChange={(e) => setName(e.target.value)} value={name}/>
+
+         <label className='label'>UserName:</label>
+         <input className='input' type='text' placeholder='inserisci il tuo User' onChange={(e) => setUsername(e.target.value)} value={username}/>
+
+         <label className='label'>Password:</label>
+         <input className='input' type='password' placeholder='inserisci la tua password' onChange={(e) => setPassword(e.target.value)} value={password}/>
+
+         <label className='label'>Specializzazione:</label>
+         <select className='input' value={specializzazione} onChange={(e) => setSpecializzazione(e.target.value)}>
+          <option value="">-- Scegli una Specializzazione --</option>
+          <option value="Full Stack">Full Stack</option>
+          <option value="Frontend">Frontend</option>
+          <option value="Backend">Backend</option>
+         </select>
+
+          <label className='label'>Anni di esperienza:</label>
+         <input className='input' type='number' placeholder='inserisci gli anni di esperienza' onChange={(e) => setAnni(e.target.value)} value={anni}/>
+
+          <label className='label'>Descrizione:</label>
+         <textarea className='input' placeholder='inserisci una breve descrizione' onChange={(e) => setDescr(e.target.value)} value={descr}/>
+
+          <button type='submit'>Invia</button>
+      </form>
     </>
   )
 }
